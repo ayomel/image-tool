1183987022c9cd78f6e517a5f667a6c625d7dfa0
Mel <huntermelverton@gmail.com>
2017-11-07 13:33:59 -0500

    added ajax with query

--- js/upload.js
+++ js/upload.js
@@ -2,11 +2,36 @@ $(document).ready(function () {
     $('.uploadBtn').click(function() {
       var myFiles = document.getElementById("browse").files;
       for (var i = 0; i < myFiles.length; i++) {
-        var fd = new FormData();
-        fd.append('file', myFiles[i]);
+        var seriesID = myFiles[i].name.split('_')[0];
+
+        getSlug(seriesID);
+        uploadFile(myFiles[i]);
+      }
+    });
+
+
+  function getSlug(ID, callback) {
+    $.ajax({
+      type:'POST',
+      url: "https://graphiti-dev-live.smithsonianearthtv.com/graphql",
+      data: '{getSeries(seriesId: "' + ID + '") { slug }}',
+      cache:false,
+      contentType: false,
+      processData: false,
+      success:function(data){
+        console.log(data.data.getSeries.slug);
+      },
+      error: function(){
+        console.log("error");
+      }
+    });
+  }
+  function uploadFile(file, slug, callback) {
+    var fd = new FormData();
+        fd.append('file', file);
         $.ajax({
             type:'POST',
-            url: "http://ae03b8058ba7111e7835e020da757784-1577133485.us-east-2.elb.amazonaws.com/test",
+            url: "http://ae03b8058ba7111e7835e020da757784-1577133485.us-east-2.elb.amazonaws.com/test/" + slug,
             data: fd,
             cache:false,
             contentType: false,
@@ -18,48 +43,5 @@ $(document).ready(function () {
                 console.log("error");
             }
         });
-      }
-      theFile.forEach(function(files) {
-        console.log(files.name.split('_')[0]);
-      })
-    });
+  }
 });
-
-// for (var i = 0; i < myFiles.length; i++) {
-// 	seriesID = myFiles[i].split(‘_’)[0]; //split the filename by _ and get the first item in the array
-//
-// 	getSlug(seriesID, function(slug) {
-// 	  //use callbacks so the upload doesn't try to go before it has a slug
-//
-// 	  //make sure we have a slug before uploading.
-// 		if (slug) {
-// 	  		uploadFile(myFiles[i], function(data) {
-// 	  			console.log(data)
-// 	  		})
-// 		}
-// 	}
-// }
-// function getSlug(seriesID, callback) {
-//
-//    // Use what you learned from the ajax call from the visualizer project
-//
-//    // make an AJAX call to https://graphiti-dev-live.smithsonianearthtv.com/graphql
-//    // with this query: getSeries(seriesId: seriesID) { slug }
-//    //success:
-//    		callback(result.getSeries.slug);
-//    //fail:
-//    		//handle errors and return false to prevent upload with out slug
-//    		callback(false);
-// }
-//
-// function uploadFile(file, slug, callback) {
-// 	var fd = new FormData();
-// 		fd.append('file', file);
-// 	// Upload ajax goes here
-// 	// USE: url: “http://ae03b8058ba7111e7835e020da757784-1577133485.us-east-2.elb.amazonaws.com/test/“+slug
-// 	//success:
-// 			callback(data);
-// 	//fail:
-// 		// handle errors
-// 		callback(false);
-// }

ba48611fbeb0c33601bbfce019d1d25d76da0175
Ava <avaposner128@gmail.com>
2017-11-10 14:55:57 -0500

    filter update

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
 $('.filter-label')
-  .on('click', function() {
+   .on('click', function() {
       $('.dropdown-list').toggle();
   })
   .on('input', '.dropdown-search', function() {
       var target = $(this);
       var search = target.val().toLowerCase();
-
+    
       if (!search) {
             $('li').show();
             return false;
         }
-
+    
       $('li').each(function() {
           var text = $(this).text().toLowerCase();
             var match = text.indexOf(search) > -1;
@@ -23,10 +23,29 @@ $('.filter-label')
   });
 
 // JSON of States for demo purposes
-var filterName = [
-    { name: 'ALABAMA'},
-    { name: 'ALASKA'},
-    { name: 'WYOMING'}
+var usStates = [
+    { name: '01', abbreviation: 'AL'},
+    { name: '02', abbreviation: 'WY' },
+    { name: '03', abbreviation: 'AL'},
+    { name: '04', abbreviation: 'AL'},
+    { name: 'NSC', abbreviation: 'AL'},
+    { name: 'FUL', abbreviation: 'AL'},
+    { name: 'GAL', abbreviation: 'AL'},
+    { name: 'GTH', abbreviation: 'AL'},
+    { name: 'SLD', abbreviation: 'AL'},
+    { name: 'SCH', abbreviation: 'AL'},
+    { name: 'SCC', abbreviation: 'AL'},
+    { name: 'DMC', abbreviation: 'AL'},
+    { name: 'EPS', abbreviation: 'AL'},
+    { name: 'SEC', abbreviation: 'AL'},
+    { name: 'SNK', abbreviation: 'AL'},
+    { name: 'PRO', abbreviation: 'AL'},
+    { name: 'WEB', abbreviation: 'AL'},
+    { name: 'BSC', abbreviation: 'AL'},
+    { name: 'INT', abbreviation: 'AL'},
+    { name: 'SMC', abbreviation: 'AL'},
+    { name: 'SCS', abbreviation: 'AL'},
+    { name: 'SES', abbreviation: 'AL'}
 ];
 
 // <li> template
@@ -38,7 +57,7 @@ var stateTemplate = _.template(
 );
 
 // Populate list with states
-_.each(filterName, function(s) {
+_.each(usStates, function(s) {
     s.capName = _.startCase(s.name.toLowerCase());
     $('ul').append(stateTemplate(s));
 });
